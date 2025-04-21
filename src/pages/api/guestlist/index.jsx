8ca34f6e8d223e198edc7252fc25ffb1b10805b8
Saurabh Kirve <>
2025-04-22 02:40:39 +0530

    ...

--- src/pages/api/guestlist/index.jsx
+++ src/pages/api/guestlist/index.jsx
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "../../auth/[...nextauth]";
-import prisma from "../../../src/lib/prisma";
+import { authOptions } from "../auth/[...nextauth]";
+import { prisma } from "@/lib/prisma";
 
 export default async function handler(req, res) {
   const session = await getServerSession(req, res, authOptions);

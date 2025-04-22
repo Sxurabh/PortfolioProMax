6eb9a850384fb5fe2672baedc0aa56cdc061606b
Saurabh Kirve <>
2025-04-22 21:35:18 +0530

    prisma

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,8 +1,10 @@
+// src/lib/prisma.js
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global;
 
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient();
+const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;

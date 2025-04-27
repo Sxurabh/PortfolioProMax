cb94f5ce3be7c5aa2b846a06545232c6dac72202
Saurabh Kirve <>
2025-04-28 00:48:00 +0530

    test

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -128,7 +128,7 @@ function Newsletter() {
           required
           className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
         />
-        <Button type="submit" className="ml-4 flex-none">
+        <Button type="submit" className="ml-4 flex-none hover:bg-teal-400 hover:text-white dark:hover:bg-teal-500 dark:hover:text-white">
           Join
         </Button>
       </div>
@@ -197,7 +197,7 @@ function Resume() {
           </li>
         ))}
       </ol>
-      <Button href="#" variant="secondary" className="group mt-6 w-full">
+      <Button href="#" variant="secondary" className="group mt-6 w-full hover:bg-teal-400 hover:text-white dark:hover:bg-teal-500 dark:hover:text-white">
         Download CV
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
@@ -303,7 +303,7 @@ export default function Home({ articles }) {
 
 export async function getStaticProps() {
   if (process.env.NODE_ENV === 'production') {
-    await generateRssFeed();
+    // await generateRssFeed();
   }
 
   let articles = [];
